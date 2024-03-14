a418dc974e1d5f2f80cf9bd753937d8b334d999e
“Gokul <arniekris@gmail.com>
2024-03-13 23:40:09 -0500

    Second commit

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-<<<<<<< HEAD
         System.out.println("Learning Java");
-=======
->>>>>>> origin/main
+        System.out.println("Testing");
     }
 }
